a25434a0afe8647c073130099b855c79ec38e032
Tom Depasse <la196807@student.helha.be>
2022-11-04 11:37:53 +0100

    animer timer

--- scripts/index.js
+++ scripts/index.js
@@ -116,3 +116,53 @@ counrtyWrapper.addEventListener('click', ({target}) => {
         showPrice(target.dataset.currency)
     }
 });
+// возвращает только слово, склонение слова
+const declOfNum = (n, titles) => titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+const timer = deadline => {
+
+    const unitDay = document.querySelector(('.timer__unit_day'));
+    const unitHour = document.querySelector(('.timer__unit_hour'));
+    const unitMin = document.querySelector(('.timer__unit_min'));
+
+    const unitDescriptionDay = document.querySelector('.timer__unit-description_day');
+    const unitDescriptionHour = document.querySelector('.timer__unit-description_hour');
+    const unitDescriptionMin = document.querySelector('.timer__unit-description_min');
+
+    const getTimeRemaning = () => {
+        const dateStop = new Date(deadline).getTime(); // in ms
+        const dateNow = Date.now(); // in ms
+        const timeRemaning = dateStop - dateNow;
+        const minutes = Math.floor( timeRemaning/ 1000 / 60 % 60);
+        const hours = Math.floor( timeRemaning/ 1000 / 60 / 60 % 24);
+        const days = Math.floor( timeRemaning/ 1000 / 60 / 60 / 24 );
+
+        return {timeRemaning, minutes, hours, days,};
+    };
+
+    const start = () => {
+        const timer = getTimeRemaning();
+
+        unitDay.textContent = timer.days;
+        unitHour.textContent = timer.hours;
+        unitMin.textContent = timer.minutes;
+        //todo ecrire en russe - 19 min lesson bonus
+        unitDescriptionDay.textContent = declOfNum(timer.days, ['', '', '']);
+        unitDescriptionHour.textContent = declOfNum(timer.hours, ['', '', '']);
+        unitDescriptionMin.textContent = declOfNum(timer.minutes, ['minuta', '', 'minut']);
+
+        const timerId = setTimeout(start, 60000);
+
+        if (timer.timeRemaning < 0) {
+            clearTimeout(timerId); // stop timer
+            unitDay.textContent = 0;
+            unitHour.textContent = 0;
+            unitMin.textContent = 0;
+        }
+    }
+
+    start();
+}
+
+timer('2023/09/07 20:00');
